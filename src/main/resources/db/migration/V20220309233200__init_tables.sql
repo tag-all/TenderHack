01e4aad0c59dd1ddab9054e795eae6ff1bbec1a0
Iuribabalin <babalin2011@gmail.com>
2022-04-15 22:51:23 +0300

    update data base migration

--- src/main/resources/db/migration/V20220309233200__init_tables.sql
+++ src/main/resources/db/migration/V20220309233200__init_tables.sql
@@ -1,16 +1,12 @@
 create sequence customer_seq start 1;
 create table customer
 (
-    id              int primary key,
-    email           varchar(128) not null,
-    email_confirmed bool default false,
-    password        varchar(255) not null,
-    name            varchar(255),
-    last_name       varchar(255),
-    patronymic      varchar(255),
-    phone           varchar(10),
-    code_region     varchar(3),
-    role            varchar(255) not null
+    id                int primary key,
+    email             varchar(128) not null,
+    password          varchar(255) not null,
+    company_name      varchar(255),
+    access_key        varchar(255),
+    notification_time time
 );
 
 create sequence token_seq start 1;
@@ -19,4 +15,34 @@ create table token
     id          int primary key,
     customer_id int references customer (id),
     token       text not null
+);
+
+create sequence bot_settings_seq start 1;
+create table bot_settings
+(
+    id          int primary key,
+    customer_id int references customer (id),
+    session_id  int    not null,
+    priority    int    not null,
+    time_delay  time   not null,
+    step        double not null,
+    min_payment double not null,
+    status_work boolean default false
+);
+
+create sequence notification start 1;
+create table message
+(
+    id           int primary key,
+    type         varchar(16) not null,
+    message_text text
+)
+
+create sequence notification_seq start 1;
+create table notification
+(
+    id          int primary key,
+    customer_id int references customer (id),
+    message_id  int references message (id),
+    read_status boolean default false
 );
