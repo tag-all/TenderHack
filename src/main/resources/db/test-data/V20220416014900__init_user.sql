ace4a68d9347a97dbb7f70b1ad2d8840d8e17b17
Iuribabalin <babalin2011@gmail.com>
2022-04-17 02:54:46 +0300

    migration

--- src/main/resources/db/test-data/V20220416014900__init_user.sql
+++ src/main/resources/db/test-data/V20220416014900__init_user.sql
@@ -1,3 +1,7 @@
 insert into customer (id, email, password, company_name, access_key, notification_time)
-values (nextval('customer_seq'), 'babalin2011@gmail.com', '$2a$10$ThV/bMKBVvZ3f6732mCuLuYhF7AVI6bIMDBNV5NwvpxtHqwGk/USq',
-        'Test', null, '01:00:00');
+values (nextval('customer_seq'), 'notificationsystemsiuri@gmail.com', '$2a$10$ThV/bMKBVvZ3f6732mCuLuYhF7AVI6bIMDBNV5NwvpxtHqwGk/USq',
+        'Поставщик бытовой техники', '2db75a1c-bd8f-11ec-9d64-0242ac120002', '01:00:00');
+
+insert into customer (id, email, password, company_name, access_key, notification_time)
+values (nextval('customer_seq'), 'fexri_imanzade@mail.ru', '$2a$10$ThV/bMKBVvZ3f6732mCuLuYhF7AVI6bIMDBNV5NwvpxtHqwGk/USq',
+        'Поставщик концелярских товаров', '2db75bca-bd8f-11ec-9d64-0242ac120002', '01:00:00');
